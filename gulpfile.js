518b21f4fb8d5620beac38a924b633e6ca48c8fa
onderakbulut <onderakbulut@yahoo.com>
2023-12-17 01:09:01 +0300

    Add slideshow section

--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,41 @@ var purgecss = require("gulp-purgecss"); // Remove Unused CSS from Styles
 // Run:
 // gulp sass
 // Complies sass only once
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     var processors = [
         tailwindcss,
         autoprefixer,
         cssnano
     ];
     return gulp.src('./sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('./assets'));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./assets'));
 });
 
-gulp.task('prod', function () {
+gulp.task('prod', function() {
     var processors = [
         tailwindcss,
         autoprefixer
     ];
     return gulp.src('./sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss(processors))
-        .pipe(purgecss({
-            content: ['./*.html', "./**/*.liquid", "./assets/*.js"]
-        }))
-        .pipe(gulp.dest('./assets'));
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(purgecss({
+        content: ['./*.html',"./**/*.liquid","./assets/*.js"],        
+        defaultExtractor: content => {
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+            const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+            return broadMatches.concat(innerMatches)
+        }
+    
+    }))
+    .pipe(gulp.dest('./assets'));
 });
 
 // Run:
 // gulp watch
 // Starts watcher and keeps compiling the css file as you make changes
 gulp.task('watch', function () {
-    gulp.watch(["./sass/*.scss", "./*.html", "./sections/*.liquid"], gulp.series('prod'));
+    gulp.watch(["./sass/*.scss","./*.html","./**/*.liquid"], gulp.series('prod'));
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,21 @@ module.exports = {
     './**/*.{html,js,liquid}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'iron': '#DCDDDF',
+      },
+    },
     container: {
       center: true,
       padding: '16px'
     },
     screens: {
       'sm': '576px',
-      'md': '768px',
-      'lg': '992px',
+      'max-md': {'max': '749px'},
+      'md': '750px',
+      'max-lg': {'max': '989px'},      
+      'lg': '990px',
       'xl': '1200px',
       'xxl': '1400px',
     }
